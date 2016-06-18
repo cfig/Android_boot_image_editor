896d6065f906de4aa91992345ea45da108f3e8fd
cfig <yuyezhong@gmail.com>
2016-06-18 22:59:38 +0800

    groovy unpack/pack tasks can run, but still failed unit test

--- abootimg/src/main/groovy/cfig/bootimg/CArgs.groovy
+++ abootimg/src/main/groovy/cfig/bootimg/CArgs.groovy
@@ -5,7 +5,7 @@ import groovy.transform.ToString
 /**
  * Created by yu at 09:57 on 2016-06-17
  */
-@ToString(includeNames = true, includeFields = true)
+@ToString(includeNames = true, includeFields = true, excludes = "toCommandLine, CArgs")
 class CArgs {
     public String kernel;
     public String ramdisk;
@@ -24,11 +24,56 @@ class CArgs {
     public int tags_offset;
     public boolean id;
 
-    CArgs() {
+    public CArgs() {
         kernel = "kernel";
         ramdisk = "ramdisk.img.gz";
         second = "second";
         output = "boot.img";
         cfg = "bootimg.json";
+    }
+
+    public List<String> toCommandList() {
+        List<String> ret = new ArrayList<String>();
+        ret.add("--base");
+        ret.add("0x" + Integer.toHexString(base));
+        ret.add("--kernel");
+        ret.add(kernel);
+        ret.add("--kernel_offset");
+        ret.add("0x" + Integer.toHexString(kernel_offset));
+        if (null != ramdisk) {
+            ret.add("--ramdisk");
+            ret.add(ramdisk);
+            ret.add("--ramdisk_offset");
+            ret.add("0x" + Integer.toHexString(ramdisk_offset));
+        }
+        if (null != second) {
+            ret.add("--second");
+            ret.add(second);
+            ret.add("--second_offset");
+            ret.add("0x" + Integer.toHexString(second_offset));
+        }
+        if (null != board) {
+            ret.add("--board");
+            ret.add(board);
+        }
+        ret.add("--cmdline");
+        ret.add(cmdline);
+        if (null != os_version) {
+            ret.add("--os_version");
+            ret.add(os_version);
+        }
+        if (null != os_patch_level) {
+            ret.add("--os_patch_level");
+            ret.add(os_patch_level);
+        }
+        ret.add("--tags_offset");
+        ret.add("0x" + Integer.toHexString(tags_offset));
+        if (id) {
+            ret.add("--id");
+        }
+        ret.add("--output");
+        ret.add(output);
+
+        return ret;
     }
 }
