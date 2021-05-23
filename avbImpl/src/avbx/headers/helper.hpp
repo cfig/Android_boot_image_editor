1cc54f4592f573c0e0a767c49ba27383dd025065
cfig <yuyezhong@gmail.com>
2021-05-23 10:37:35 +0800

    add license header for each source file
    This doesn't affect use of the software in any means under the terms of
    Apache License

--- avbImpl/src/avbx/headers/helper.hpp
+++ avbImpl/src/avbx/headers/helper.hpp
@@ -1,9 +1,16 @@
-/*
- * helper.h
- * Copyright (C) 2020 yu <yu@X.local>
- *
- * Distributed under terms of the MIT license.
- */
+// Copyright 2021 yuyezhong@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef HELPER_H
 #define HELPER_H
